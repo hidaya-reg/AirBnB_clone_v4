fe36f2570d6f9231be8f274b7047d52610958cbe
hidaya <hidaya.regragui@gmail.com>
2024-06-06 16:40:23 +0100

    task4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    const selectedAmenities = {};
+
+    $('input[type="checkbox"]').change(function() {
+        if (this.checked) {
+            selectedAmenities[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete selectedAmenities[$(this).data('id')];
+        }
+        
+        const amenityNames = Object.values(selectedAmenities);
+        $('div.amenities h4').text(amenityNames.join(', '));
+    });
+
+    $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+        if (data.status === 'OK') {
+            $('#api_status').addClass('available');
+        } else {
+            $('#api_status').removeClass('available');
+        }
+    });
+
+    $.ajax({
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({}),
+        success: function(data) {
+            for (let place of data) {
+                $('section.places').append(
+                    `<article>
+                        <div class="title_box">
+                            <h2>${place.name}</h2>
+                            <div class="price_by_night">$${place.price_by_night}</div>
+                        </div>
+                        <div class="information">
+                            <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                            <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                            <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+                        </div>
+                        <div class="description">
+                            ${place.description}
+                        </div>
+                    </article>`
+                );
+            }
+        }
+    });
+});
